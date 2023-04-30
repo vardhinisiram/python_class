6efc6795d4a48a8d1612ce57ef1472057698cdc6
vardhinisiram <vardhinisiram@gmail.com>
2023-04-30 16:29:14 +0530

    rewrittten binary and solved codechef problems

--- binary1.py
+++ binary1.py
@@ -1,12 +1,18 @@
 a=[20,25,30,35,40,45]
-x=int(input("enter the number"))
-
-half=(0+len(a))//2
-if x==a[half]:
-    print(x,"is in list")
-if x>half:
-    half=(half+len(a))//2
-    if x==half:
-        print (x,"in the list")
+x=int(input("enter the number: "))
+found=True
+while len(a)!=1:
+    mid=(0+len(a))//2
+    if x==a[mid]:
+        print(x," is in the list")
+        break
+    found=False
+    if x>a[mid]:
+        a=a[mid:]
+    elif x<a[mid]:
+        a=a[:mid]
+if found==False:
+    print(x," is not in the list")
+    
 
 
